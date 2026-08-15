d22427fc5ad65099fa402e103917709771a61ac1
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2026-08-15 12:35:14 -0700

    feat: package T3 Code for Darwin

---
+++ pkgs/by-name/t3/t3-code/source.nix
@@ -0,0 +1,7 @@
+{
+  version = "0.0.33";
+  src = {
+    url = "https://github.com/pingdotgg/t3code/releases/download/v0.0.33/T3-Code-0.0.33-arm64.zip";
+    hash = "sha256-swT6pUzboWnveUH3WmlCMpxAmMVFGUYBmDBHFJmzq7s=";
+  };
+}
