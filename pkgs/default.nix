5a7d36e92081e0b5d98b1aaf5f6df600e34c3b39
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2026-08-15 22:43:14 -0700

    revert: remove Spotify Xirp package

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -13,7 +13,6 @@ let
     spotify-spotx = callPackage ./by-name/sp/spotify-spotx/package.nix { };
     steam = callPackage ./by-name/st/steam/package.nix { };
     t3-code = callPackage ./by-name/t3/t3-code/package.nix { };
-    xirp = callPackage ./by-name/xi/xirp/package.nix { };
   };
 in
 {
