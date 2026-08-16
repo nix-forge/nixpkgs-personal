44305804da4dcf8f7248579d1b87364ada65e1ec
Ian Holloway <72767437+IanHollow@users.noreply.github.com>
2026-08-16 16:42:59 +0000

    revert: remove Spotify Xirp package (#18)

--- pkgs/by-name/xi/xirp/source.nix
+++
@@ -1,8 +0,0 @@
-{
-  version = "0.14.0";
-  appName = "Xirp";
-  src = {
-    url = "https://reckless-finch.spotifycdn.com/external/Xirp-0.14.0-arm64-external.zip";
-    hash = "sha256-LULuZx7+s/IDfvWXHM5kGuKXLLie58t3C8pEYlzx2mc=";
-  };
-}
